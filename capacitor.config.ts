57f911efab2b8f5ee4f877dae36bfeda5cc2d720
Jacob Chapman <chapmajaco@ecs.vuw.ac.nz>
2022-09-23 21:11:17 +1200

    Add Native Android building

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'activitytracker',
   appName: 'Activity Tracker',
   webDir: 'www',
   bundledWebRuntime: false
