9121086c3812fa435992852084db7fbf63fe5a0f
Jacob Chapman <chapmajaco@ecs.vuw.ac.nz>
2022-09-23 19:04:56 +1200

    Working geolocation and trying to get device motion to work next

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Geolocation, Position} from '@capacitor/geolocation';
+import {Motion} from '@capacitor/motion';
+import {PluginListenerHandle} from '@capacitor/core';
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  coords: Position['coords'];
+  accelHandler: PluginListenerHandle;
+  hello: any = 'before-er';
 
-  constructor() {}
+  constructor() {
+  }
 
+  async locate() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.coords = coordinates.coords;
+  }
+
+  async watch() {
+    this.hello = 'before';
+    //if ( device is ios do this)
+    //await (DeviceMotionEvent as any).requestPermission();
+    this.hello = 'hello';
+
+    // Once the user approves, can start listening:
+    this.accelHandler = await Motion.addListener('accel', event => {
+      console.log('Device motion event:', event);
+      this.hello = 'evented';
+    });
+  }
 }
