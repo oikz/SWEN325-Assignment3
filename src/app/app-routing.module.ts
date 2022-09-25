32c085c62a66527a2949f9f9163161a3a41e3835
Jacob Chapman <chapmajaco@ecs.vuw.ac.nz>
2022-09-25 13:46:12 +1300

    Add Firebase and set up placeholder pages and routes for Sign in/Sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    loadChildren: () => import('./auth/welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
+  {
+    path: 'signin',
+    loadChildren: () => import('./auth/signin/signin.module').then(m => m.SigninPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./auth/signup/signup.module').then(m => m.SignupPageModule)
+  },
+  {
+    path: 'home',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
 ];
 @NgModule({
   imports: [
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'swen325assignment3-ca2e1',
+    appId: '1:791976683499:web:ef51fd1814ba6c2e54dbae',
+    storageBucket: 'swen325assignment3-ca2e1.appspot.com',
+    apiKey: 'AIzaSyAmxFqO4WOfwZw4sOIIzQFuFbGqvVmlk8I',
+    authDomain: 'swen325assignment3-ca2e1.firebaseapp.com',
+    messagingSenderId: '791976683499',
+    measurementId: 'G-TC4187D3LE',
+  },
   production: false
 };
 
