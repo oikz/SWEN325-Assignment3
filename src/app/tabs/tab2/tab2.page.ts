b798fdce00a50bdd92eabf88ac1eae3c53d454b4
Jacob Chapman <chapmajaco@ecs.vuw.ac.nz>
2022-09-26 17:53:05 +1300

    Working Official Capacitor Google Maps
    - Might want to change to community for more features

--- src/app/tabs/tab2/tab2.page.ts
+++ src/app/tabs/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {GoogleMap} from '@capacitor/google-maps';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  @ViewChild('map') mapRef: ElementRef;
+  map: GoogleMap;
 
-  constructor() {}
+  constructor() {
 
+  }
+
+  ionViewDidEnter() {
+    this.createMap();
+  }
+
+  async createMap() {
+    this.map = await GoogleMap.create({
+      id: 'my-map',
+      apiKey: 'AIzaSyAZfmwK5h1tQgPlYQT2cwe6JVCtzQWRxdE',
+      config: {
+        center: {
+          lat: -41.2924,
+          lng: 174.7784
+        },
+        zoom: 8
+      },
+      element: this.mapRef.nativeElement,
+    });
+  }
 }
