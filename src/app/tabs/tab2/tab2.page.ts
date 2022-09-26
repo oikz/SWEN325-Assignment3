e2b0242e482abed3a1f04dddc7c7594761481ff8
Tarik Kurnaz <kurnaztari@myvuw.ac.nz>
2022-09-26 22:52:52 +1300

    Adding firestore to project

--- src/app/tabs/tab2/tab2.page.ts
+++ src/app/tabs/tab2/tab2.page.ts
@@ -1,6 +1,9 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import {CapacitorGoogleMaps} from '@capacitor-community/capacitor-googlemaps-native';
 import {Geolocation} from '@capacitor/geolocation';
+import {Observable} from 'rxjs';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/compat/firestore';
+import {AngularFireAuth} from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-tab2',
@@ -8,11 +11,27 @@ import {Geolocation} from '@capacitor/geolocation';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
   @ViewChild('map') mapView: ElementRef;
+
+  locations: Observable<any>;
+  locationsCollection: AngularFirestoreCollection<any>;
+  user = null;
+  isTracking = false;
+  watch = null;
+
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
+    this.afAuth.onAuthStateChanged((user) => {
+      this.user = user;
+      this.locationsCollection = this.afs.collection(
+        'locations/' + user.uid + '/locations',
+        ref => ref.orderBy('timestamp', 'desc')
+      );
+    });
+  }
 
   ionViewDidEnter() {
     this.createMap();
+    this.loadMap();
   }
 
   async createMap() {
@@ -47,5 +66,41 @@ export class Tab2Page {
 
   ionViewDidLeave() {
     CapacitorGoogleMaps.close();
+  }
+
+  loadMap() {
+    this.locations = this.locationsCollection.valueChanges();
+    this.locations.subscribe((locations) => {
+      locations.forEach((location) => {
+        CapacitorGoogleMaps.addMarker({
+          latitude: location.latitude,
+          longitude: location.longitude,
+          title: location.timestamp,
+        });
+      });
+    });
+  }
+
+  startTracking() {
+    this.isTracking = true;
+    this.watch = Geolocation.watchPosition({}, (position, err) => {
+      console.log('new position' + position);
+      if (err) {
+        console.log(err);
+        return;
+      }
+      this.locationsCollection.add({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        timestamp: new Date().getTime(),
+      });
+    });
+  }
+
+  // Unsubscribe from the geolocation watch using the initial ID
+  stopTracking() {
+    Geolocation.clearWatch({id: this.watch}).then(() => {
+      this.isTracking = false;
+    });
   }
 }
